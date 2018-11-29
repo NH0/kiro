71b4330ed770aad20b425a13b753b28a29669660
T-F <85ca4663@opayq.com>
2018-11-29 17:15:14 +0100

    code.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import code, kruskal
+import "code.py"
+import kruskal
 
 distances_matrix = code.read_distances_csv(13)
 distribution_tab, terminal_tab = code.read_nodes_csv()
@@ -7,11 +8,7 @@ distribution_tab, terminal_tab = code.read_nodes_csv()
 g = kruskal.Graph(n)
 for i in range(n) :
     for j in range(n) :
-<<<<<<< HEAD
-        g.addEdge(i, j, distances_matrix[i][j])
-=======
         g.addEdge(i, j, distances_matrix[j][i])
->>>>>>> 51aaafc9bb4e8c718f9d90305312cbc86c3e3adc
 min_span = g.KruskalMST()
 
 print(min_span)
