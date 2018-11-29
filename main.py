7ef1125e25f6d68ff22d5ef4ae5ab80378623528
idontknowp <idontknowp@users.noreply.github.com>
2018-11-29 17:09:30 +0100

    sd

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import code, kruskal
 
-distances_matrix = read_distances_csv(13)
-distribution_tab, terminal_tab = read_nodes_csv()
+distances_matrix = code.read_distances_csv(13)
+distribution_tab, terminal_tab = code.read_nodes_csv()
 
-def init_graph(n, distances_matrix=distances_matrix, nodes_tab=terminal_tab+distribution_tab) :
+#minimum spanning
+g = kruskal.Graph(n)
+for i in range(n) :
+    for j in range(n) :
+        g.addEdge(i, j, distances_matrix[i,j])
+min_span = g.KruskalMST()
 
-    g = Graph(n)
-    for i in range(n) :
-        for j in range(n) :
-            g.addEdge(i, j, distances_matrix[i,j])
-     return g
 
-def min_spanning(g) :
-    return g.KruskalMST()
+
+
+
+
 
