c29a11349ca700cd102a3f9069fa4a1a9c16fdc3
T-F <85ca4663@opayq.com>
2018-11-29 18:37:42 +0100

    dd

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ distances_matrix = read_distances_csv(n)
 distribution_tab, terminal_tab = read_nodes_csv()
 k, l = len(distribution_tab), len(terminal_tab)
 
+listeDansCycle = []
+
 #minimum spanning
 g = Graph(n)
 for i in range(n) :
@@ -29,18 +31,20 @@ def nearest_vertice(i, listeNonVisit, distances_matrix = distances_matrix, k = k
 
 def create_cycle(idDistri, listeNonVisit):
     listebi = [idDistri]
+    listeDansCycle.append(idDistri)
     i=idDistri;
     nbElements=0;
     while (len(listeNonVisit)>0 and nbElements<29):
         j = nearest_vertice(i, listeNonVisit)
         listeNonVisit.remove(j)
         listebi.append(j)
+        listeDansCycle.append(j)
         i=j
         nbElements+=1
     return listebi
 
 def main():
-    listeNonVisit = [i for i in range k+l]
+    listeNonVisit = [i for i in range(k,k+l)]
     nbDistri = k
     listeb = []
     while (len(listeNonVisit)>0 and nbDistri>0):
